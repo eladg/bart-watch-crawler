47eda925ce73d4344610c4516ae1732c6b634352
eladg <ggrelad@gmail.com>
2017-05-11 10:18:01 -0700

    decouple client

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
-var webpack = require('webpack');
-var path    = require('path');
-var fs      = require('fs');
-var nodeExternals = require('webpack-node-externals');
+const webpack           = require('webpack');
+const path              = require('path');
+const nodeExternals     = require('webpack-node-externals');
 
-var nodeModulesPath  = path.resolve(__dirname, 'node_modules');
-var buildPath        = path.resolve(__dirname, 'build');
-
-var crawlerApp       = path.resolve(__dirname, 'crawler', 'crawler.js');
+var nodeModulesPath     = path.resolve(__dirname, 'node_modules');
+var buildPath           = path.resolve(__dirname, 'build');
+var crawlerApp          = path.resolve(__dirname, 'crawler', 'crawler.js');
 
 var config = {
   name: 'bart-watch',
   target: 'node',
-  devtool: 'sourcemap',
+  devtool: 'source-map',
 
   entry: {
     crawler: crawlerApp,
@@ -23,16 +21,6 @@ var config = {
   },
 
   externals: [nodeExternals()],
-
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: [nodeModulesPath]
-      },
-    ]
-  },
 };
 
 module.exports = config;
