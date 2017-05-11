e17d71465e630e295850a9eca1b39db0ce306c18
eladg <ggrelad@gmail.com>
2017-05-10 21:18:17 -0700

    crawler s3-writes

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -5,27 +5,23 @@ import http from "http";
 import fs from "fs";
 import path from "path";
 
-import { mkdirParent } from "../shared/utils";
+import aws from 'aws-sdk';
+
+import dotenv from "dotenv"; dotenv.config();
 
 function log(msg) {
   console.log(Date.now() + ": " + msg);
 }
 
-function error(msg) {
-  console.error(Date.now() + ": " + msg);
-}
-
-function filenameForCode(code) {
-  return `${config.data_path}/${code}/${Date.now()}.json`;
+function log_error(msg) {
+  console.error(Date.now() + ": !! " + msg);
 }
 
 function fetchAndSave(source) {
 
   const code = source.code;
   const api_url = source.api_url;
-  const filepath = filenameForCode(code);
-
-  mkdirParent(path.dirname(filepath));
+  const s3key = `data/${code}/${Date.now()}.json`;
 
   return http.get(api_url, (res) => {
     const { statusCode } = res;
@@ -42,7 +38,7 @@ function fetchAndSave(source) {
     }
 
     if (error) {
-      console.error(error.message);
+      log_error(error.message);
 
       // consume response data to free up memory
       res.resume();
@@ -51,27 +47,42 @@ function fetchAndSave(source) {
 
     res.setEncoding('utf8');
     let rawData = '';
+
     res.on('data', (chunk) => { rawData += chunk; });
+
     res.on('end', () => {
       try {
+
         let parser = new xml2js.Parser();
         parser.parseString(rawData, (err, result) => {
           const data = JSON.stringify(result);
-          log(`>> ${filepath} parsed`);
+          log(`>> ${s3key} parsed`);
 
-          fs.writeFile(filepath, data, (err) => {
-            if(err) { throw err; }
-            log(`>> ${filepath} saved!`);
+          const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+          const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+          aws.config.update({
+            accessKeyId,
+            secretAccessKey,
           });
+
+          let s3 = new aws.S3();
+          const params = {Bucket: config.s3_bucket, Key: s3key, Body: data};
+          s3.putObject(params, function(err, data) {
+            if (err) { return log_error(err); }
+
+            log(`>> ${s3key} saved!`);
+          });
+
         });
 
       } catch (e) {
-        error(e.message);
+        log_error(e.message);
       }
 
     });
   }).on('error', (e) => {
-    error(`Got error: ${e.message}`);
+    log_error(`Got error: ${e.message}`);
   });
 }
 
@@ -81,5 +92,5 @@ function crawl() {
   });  
 }
 
-log("Crawler: Start");
+log(">> Crawler: Start");
 crawl();
